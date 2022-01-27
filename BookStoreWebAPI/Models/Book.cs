ae693b21841718c0b5388a547f53f43dd5c29c37
zeeshanliaqat50 <zeeshanliaqat90@gmail.com>
2022-01-27 22:08:42 +0500

    Model Validations added

--- BookStoreWebAPI/Models/Book.cs
+++ BookStoreWebAPI/Models/Book.cs
@@ -1,10 +1,13 @@
-﻿namespace BookStoreWebAPI.Data
+﻿using System.ComponentModel.DataAnnotations; //this package is used for model validations which mapped to database
+namespace BookStoreWebAPI.Data
 {
     public class Book
     {
+        
         public int id { get; set; }
+        [Required(ErrorMessage ="Title required")]
         public String title { get; set; }
-
+        [Required(ErrorMessage ="Description required")]
         public String description { get; set; }
 
     }
